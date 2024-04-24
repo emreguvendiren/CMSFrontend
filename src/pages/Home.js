57c8faa20241922f2752fe569f426bcb7bded2fa
FURKAN İÇÖZ <furkanicoz@outlook.com>
2024-04-25 00:24:40 +0300

    useeffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { getRequest } from "../services/apiService";
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 
 export default function Home(){
 
     const [userName,setUsername] = useState(); 
+    const [password,setPassword] = useState(); 
+
 
     useEffect(()=>{
         getRequest("test/hello",(responseData)=>{
             console.log(responseData);
         });
-    });
+    },[userName]);
 
     return(
         <div>
             <TextField 
-                            label="Takip Numarası" 
-                            sx={{mb:2,width:'25%'}}
-                            value={userName}
-                            onChange={(event)=>{setUsername(event.target.value)}}
-                            > </TextField>
+                label="Takip Numarası" 
+                sx={{mb:2,width:'25%'}}
+                value={userName}
+                onChange={(event)=>{setUsername(event.target.value)}}
+         > </TextField>
+
+            <Button 
+                variant="contained"
+                onClick={()=>{
+                    console.log(userName)
+                }}
+            >
+                BAS
+            </Button>
+            
+            <TextField 
+                label="emreguvcendiden" 
+                sx={{mb:2,width:'25%'}}
+                value={password}
+                onChange={(event)=>{setPassword(event.target.value)}}
+         > </TextField>
+
+            <Button 
+                variant="contained"
+                onClick={()=>{
+                    console.log(password)
+                }}
+            >
+                BASma
+            </Button>
+                
         </div>
+        
 
     );
 }
