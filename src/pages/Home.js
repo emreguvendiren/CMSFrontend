49c3963039fe8ca99e56b5667fb22fa876311eb7
FURKAN İÇÖZ <furkanicoz@outlook.com>
2024-04-25 00:13:47 +0300

    MaterialUI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getRequest } from "../services/apiService";
+import { TextField } from "@mui/material";
 
 
 export default function Home(){
+
+    const [userName,setUsername] = useState(); 
 
     useEffect(()=>{
         getRequest("test/hello",(responseData)=>{
             console.log(responseData);
         });
-    },[]);
+    });
 
     return(
         <div>
-            Hello World
+            <TextField 
+                            label="Takip Numarası" 
+                            sx={{mb:2,width:'25%'}}
+                            value={userName}
+                            onChange={(event)=>{setUsername(event.target.value)}}
+                            > </TextField>
         </div>
+
     );
 }
