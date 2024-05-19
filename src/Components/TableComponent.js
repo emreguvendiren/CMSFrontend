c9aeffef66e5355b3cacd12764ff36412b31d544
emreguvendiren <guvendirenemre1@gmail.com>
2024-05-20 00:25:09 +0300

    Table Component Change

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -1,15 +1,15 @@
 import { Button, Card, CardActionArea, CardContent, CardHeader, Typography } from "@mui/material";
 
-function TableComponent({TableName}){
+function TableComponent({Table,handleTableClick}){
 
     const handleClick = () =>{
-        console.log(`Masa ${TableName}`);
+        //console.log(Table);
     }
     return(
         <Card sx={{width:240, maxWidth: 200, m: 2 }}>
-            <CardActionArea onClick={handleClick} sx={{height:'100%'}}>
+            <CardActionArea onClick={()=>{handleTableClick(Table)}} sx={{height:'100%'}}>
             <CardHeader
-                    title={`MASA ${TableName}`}
+                    title={`MASA ${Table.tableName}`}
                     titleTypographyProps={{ variant: 'h6', fontWeight: 'bold', textAlign: 'center' }}
                 />
                 <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
--- src/pages/Tables/Tables.js
+++ src/pages/Tables/Tables.js
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from "react";
-import TableComponent from "./TableComponent";
+import TableComponent from "../../Components/TableComponent"
 import { getRequest } from "../../services/apiService";
 import { useAuth } from "../../hooks/auth";
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import Modal from 'react-modal';
 
+const customStyles = {
+    content: {
+        position: 'absolute',
+        height: 600,
+
+        border: '3px solid #B4B4B8',
+        
+        top: '27%',
+        left: '40%',
+        marginRight: '-20%',
+        transform: 'translate(-27%, -15%)',
+        right:'30%',
+        borderRadius :'15px'
+    },
+};
 
 export default function Tables(){
     const {cookies} = useAuth();
     const [tables,setTables] = useState([]);
+    const [modalVisible,setModalVisible] = useState(false);
+    const [selectedTable,setSelectedTable] = useState(null);
 
     useEffect(()=>{
 
@@ -17,13 +35,36 @@ export default function Tables(){
 
     },[])
 
+    const handleTableClick = async(table)=>{
+        setSelectedTable(table);
+        setModalVisible(true);
+    }
+
     return(
         <Grid container spacing={2} justifyContent="flex-start">
             {tables.map((item, index) => (
                 <Grid item key={index} xs={12} sm={6} md={4} lg={2.4}>
-                    <TableComponent TableName={item.tableName} />
+                    <TableComponent Table={item} handleTableClick={handleTableClick} />
                 </Grid>
             ))}
+
+            <Modal
+             style={customStyles}
+             isOpen={modalVisible}
+             disableBackdropClick={false}
+             >
+                <div>
+                    <div style={{marginBottom:10}}>
+                        <Button onClick={()=>{setModalVisible(false)}}>Kapat</Button>
+                    </div>
+                    <div>
+                        {
+                            selectedTable!=null &&
+                                selectedTable.tableName
+                        }
+                    </div>
+                </div>
+             </Modal>
         </Grid>
     )
 }
